7238cd08d884449a28819f555570128314df9b74
loveyourstack <loveyourstack@gmail.com>
2024-08-19 16:20:57 +0200

    remove SortEqual, add EqualUnordered

--- lysslices/base.go
+++ lysslices/base.go
@@ -5,9 +5,24 @@ import (
 	"slices"
 )
 
-// SortEqual sorts s1 and s2 then returns true if they are equal
-func SortEqual[S ~[]E, E cmp.Ordered](s1, s2 S) bool {
-	slices.Sort(s1)
-	slices.Sort(s2)
-	return slices.Equal(s1, s2)
+// EqualUnordered true if s1 and s2 are equal regardless of sorting
+func EqualUnordered[S ~[]E, E cmp.Ordered](s1, s2 S) bool {
+
+	if len(s1) != len(s2) {
+		return false
+	}
+
+	s1s := make([]E, len(s1))
+	for _, v := range s1 {
+		s1s = append(s1s, v)
+	}
+
+	s2s := make([]E, len(s2))
+	for _, v := range s2 {
+		s2s = append(s2s, v)
+	}
+
+	slices.Sort(s1s)
+	slices.Sort(s2s)
+	return slices.Equal(s1s, s2s)
 }
--- lysslices/base_test.go
+++ lysslices/base_test.go
@@ -6,26 +6,36 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSortEqualSuccess(t *testing.T) {
+func TestEqualUnorderedSuccess(t *testing.T) {
 	s1 := []int{1, 2, 3}
 	s2 := []int{3, 1, 2}
-	eq := SortEqual(s1, s2)
+	eq := EqualUnordered(s1, s2)
 	assert.Equal(t, true, eq)
 
 	sa := []string{"a", "b", "c"}
 	sb := []string{"c", "a", "b"}
-	eq = SortEqual(sa, sb)
+	eq = EqualUnordered(sa, sb)
 	assert.Equal(t, true, eq)
 }
 
-func TestSortEqualFailure(t *testing.T) {
+func TestEqualUnorderedFailure(t *testing.T) {
 	s1 := []int{1, 2, 3}
 	s2 := []int{1, 2}
-	eq := SortEqual(s1, s2)
+	eq := EqualUnordered(s1, s2)
+	assert.Equal(t, false, eq)
+
+	s3 := []int{1, 2, 3}
+	s4 := []int{1, 2, 4}
+	eq = EqualUnordered(s3, s4)
 	assert.Equal(t, false, eq)
 
 	sa := []string{"a", "b", "c"}
 	sb := []string{"a", "b"}
-	eq = SortEqual(sa, sb)
+	eq = EqualUnordered(sa, sb)
+	assert.Equal(t, false, eq)
+
+	sc := []string{"a", "b", "c"}
+	sd := []string{"a", "b", "d"}
+	eq = EqualUnordered(sc, sd)
 	assert.Equal(t, false, eq)
 }
