be03bbb50307ecff28a4f2abaccd2751c5b8c248
loveyourstack <loveyourstack@gmail.com>
2026-01-13 12:56:15 +0100

    add ValueMap func

---
+++ lyspg/value_map.go
@@ -0,0 +1,37 @@
+package lyspg
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/loveyourstack/lys/lyserr"
+)
+
+// ValueMap returns of map of k = keyCol, v = valCol from the supplied table. KeyCol must be unique.
+// whereClause is optional. If passed, it must start with " AND ..."
+func ValueMap[keyT comparable, valT any](ctx context.Context, db PoolOrTx, schemaName, tableName, keyCol, valCol, whereClause string) (m map[keyT]valT, err error) {
+
+	type s struct {
+		K keyT
+		V valT
+	}
+
+	stmt := fmt.Sprintf("SELECT %s, %s FROM %s.%s WHERE 1=1%s;", keyCol, valCol, schemaName, tableName, whereClause)
+	rows, _ := db.Query(ctx, stmt)
+	items, err := pgx.CollectRows(rows, pgx.RowToStructByPos[s])
+	if err != nil {
+		return nil, lyserr.Db{Err: fmt.Errorf("pgx.CollectRows failed: %w", err), Stmt: stmt}
+	}
+
+	m = make(map[keyT]valT)
+	for _, item := range items {
+		m[item.K] = item.V
+	}
+
+	if len(items) != len(m) {
+		return nil, fmt.Errorf("key is not unique. len(items) is %v, but len(m) is %v", len(items), len(m))
+	}
+
+	return m, nil
+}
