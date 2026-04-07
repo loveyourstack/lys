ff1b9de85070e6bcb95511d2b46421af83f3c785
loveyourstack <loveyourstack@gmail.com>
2026-04-07 12:08:39 +0200

    tidy and add ctx to Run func

--- lysexec/base.go
+++ lysexec/base.go
@@ -2,44 +2,28 @@ package lysexec
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"io"
 	"os"
 	"os/exec"
 )
 
-// Run is a wrapper for exec.Cmd.Run() which includes the results of Stdout and Stderr in case of error
-func Run(name string, args ...string) (out string, err error) {
+// Run is a wrapper for exec.CommandContext.Run which includes the results of Stdout and Stderr in every case
+func Run(ctx context.Context, writeStdOut bool, name string, args ...string) (stdOut, stdErr string, err error) {
 
-	cmd := exec.Command(name, args...)
-
-	var outB, errB bytes.Buffer
-	cmd.Stdout = &outB
-	cmd.Stderr = &errB
-	if err = cmd.Run(); err != nil {
-		if errB.String() != "" {
-			return "", fmt.Errorf("cmd.Run failed: stdout: %s, stderr: %s", outB.String(), errB.String())
-		}
-		return "", fmt.Errorf("cmd.Run failed (no stderr): %w", err)
-	}
-
-	return outB.String(), nil
-}
-
-// RunWithStdout is a wrapper for exec.Cmd.Run() which includes the results of Stdout and Stderr in case of error. It continues to write Stdout as normal.
-func RunWithStdout(name string, args ...string) (out string, err error) {
-
-	cmd := exec.Command(name, args...)
+	cmd := exec.CommandContext(ctx, name, args...)
 
 	var outB, errB bytes.Buffer
-	cmd.Stdout = io.MultiWriter(os.Stdout, &outB)
+	if writeStdOut {
+		cmd.Stdout = io.MultiWriter(os.Stdout, &outB)
+	} else {
+		cmd.Stdout = &outB
+	}
 	cmd.Stderr = &errB
 	if err = cmd.Run(); err != nil {
-		if errB.String() != "" {
-			return "", fmt.Errorf("cmd.Run failed: stdout: %s, stderr: %s", outB.String(), errB.String())
-		}
-		return "", fmt.Errorf("cmd.Run failed (no stderr): %w", err)
+		return outB.String(), errB.String(), fmt.Errorf("cmd.Run failed: %s", errB.String())
 	}
 
-	return outB.String(), nil
+	return outB.String(), errB.String(), nil
 }
