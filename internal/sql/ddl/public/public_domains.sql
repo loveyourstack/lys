c94eb0e145a72d8b06f7f9f941c2b6ab71215b55
loveyourstack <loveyourstack@gmail.com>
2026-04-17 12:40:50 +0200

    move conn funcs to own file

--- internal/sql/ddl/public/public_domains.sql
+++ internal/sql/ddl/public/public_domains.sql
@@ -3,5 +3,5 @@ CREATE DOMAIN text_medium AS varchar(256) NOT NULL;
 CREATE DOMAIN text_medium_mandatory AS varchar(256) NOT NULL CHECK (value != '');
 
 CREATE DOMAIN tracking_at AS timestamp with time zone NOT NULL DEFAULT now();
-CREATE DOMAIN tracking_created_by AS text_medium_mandatory NOT NULL DEFAULT 'Unknown';
-CREATE DOMAIN tracking_last_user_update_by AS text_medium NOT NULL DEFAULT '';
+CREATE DOMAIN tracking_created_by AS text_medium_mandatory DEFAULT 'Unknown';
+CREATE DOMAIN tracking_last_user_update_by AS text_medium DEFAULT '';
--- lyspgdb/base.go
+++ lyspgdb/base.go
@@ -8,8 +8,12 @@ import (
 	"io/fs"
 	"log/slog"
 
-	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	DbSizePrettyStmt = "SELECT pg_size_pretty(pg_database_size(current_database()));"
+	VersionStmt      = "SELECT version();"
 )
 
 // Database holds postgres database config options
@@ -24,109 +28,6 @@ type Database struct {
 type User struct {
 	Name     string `toml:"userName"`
 	Password string
-}
-
-func getConnStr(dbConfig Database, userConfig User, appName string) string {
-	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?application_name=%s", userConfig.Name, userConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Database, appName)
-}
-
-// GetConn returns a connection pool to the postgres database matching the config params
-func GetConfig(dbConfig Database, userConfig User, appName string) (cfg *pgxpool.Config, err error) {
-
-	cfg, err = pgxpool.ParseConfig(getConnStr(dbConfig, userConfig, appName))
-	if err != nil {
-		return nil, fmt.Errorf("pgxpool.ParseConfig failed: %w", err)
-	}
-
-	return cfg, nil
-}
-
-// GetPool returns a connection pool to the postgres database matching the config params
-func GetPool(ctx context.Context, dbConfig Database, userConfig User, appName string) (db *pgxpool.Pool, err error) {
-
-	db, err = pgxpool.New(ctx, getConnStr(dbConfig, userConfig, appName))
-	if err != nil {
-		return nil, fmt.Errorf("pgxpool.New failed: %w", err)
-	}
-
-	return db, nil
-}
-
-// GetPoolWithTypes returns a connection pool and registers the supplied types to each connection
-func GetPoolWithTypes(ctx context.Context, dbConfig Database, userConfig User, appName string, dataTypeNames []string) (db *pgxpool.Pool, err error) {
-
-	cfg, err := GetConfig(dbConfig, userConfig, appName)
-	if err != nil {
-		return nil, fmt.Errorf("GetConfig failed: %w", err)
-	}
-
-	// register types in conn AfterConnect hook
-	cfg.AfterConnect = func(ctx context.Context, conn *pgx.Conn) error {
-		for _, typeName := range dataTypeNames {
-			dataType, err := conn.LoadType(ctx, typeName)
-			if err != nil {
-				return fmt.Errorf("conn.LoadType failed for type: %s: %w", typeName, err)
-			}
-			conn.TypeMap().RegisterType(dataType)
-		}
-		return nil
-	}
-
-	db, err = pgxpool.NewWithConfig(ctx, cfg)
-	if err != nil {
-		return nil, fmt.Errorf("pgxpool.NewWithConfig failed: %w", err)
-	}
-
-	return db, nil
-}
-
-type ContextKey string
-
-// GetPoolWithCtxSetting returns a connection pool wherein each connection has a setting from ctx applied to it on acquisition
-// adapted from https://github.com/jackc/pgx/issues/288
-func GetPoolWithCtxSetting[ctxValueT any](ctx context.Context, dbConfig Database, userConfig User, appName, settingName string, ctxKey ContextKey, errorLog *slog.Logger) (db *pgxpool.Pool, err error) {
-
-	cfg, err := GetConfig(dbConfig, userConfig, appName)
-	if err != nil {
-		return nil, fmt.Errorf("GetConfig failed: %w", err)
-	}
-
-	cfg.BeforeAcquire = func(ctx context.Context, conn *pgx.Conn) bool {
-
-		ctxVal, ok := ctx.Value(ctxKey).(ctxValueT)
-		if !ok {
-			errorLog.Error("ctxVal not found in ctx")
-			return false
-		}
-
-		// set ctx value into this connection's setting
-		_, err := conn.Exec(ctx, fmt.Sprintf("SET %s TO %v;", settingName, ctxVal))
-		if err != nil {
-			errorLog.Error("conn.Exec (set ctx value) failed: " + err.Error())
-			return false
-		}
-
-		return true
-	}
-
-	cfg.AfterRelease = func(conn *pgx.Conn) bool {
-
-		// unset the setting before this connection is released to pool
-		_, err := conn.Exec(ctx, fmt.Sprintf("SET %s TO '';", settingName))
-		if err != nil {
-			errorLog.Error("conn.Exec (unset ctx value) failed: " + err.Error())
-			return false
-		}
-
-		return true
-	}
-
-	db, err = pgxpool.NewWithConfig(ctx, cfg)
-	if err != nil {
-		return nil, fmt.Errorf("pgxpool.NewWithConfig failed: %w", err)
-	}
-
-	return db, nil
 }
 
 // ExecuteFile extracts the supplied file from the supplied filesystem and executes it into supplied database
